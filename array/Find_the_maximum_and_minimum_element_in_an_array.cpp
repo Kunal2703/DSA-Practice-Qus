e4944a7aaea49d3f55cedb64acf593551a949cb3
KUNAL <78562069+Kunal2703@users.noreply.github.com>
2022-01-23 15:49:33 +0530

    Add files via upload

---
+++ array/Find_the_maximum_and_minimum_element_in_an_array.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int SizeofArray, array[100], i, max, min;
+    cout<<"Enter the Size of the array: ";
+    cin >> SizeofArray;
+
+    cout<<"Enter the element of the Array: ";
+    for(i = 0; i < SizeofArray; i++)
+    {
+        cin >> array[i];
+    }
+    cout<<"\nThe Element of the Array is: ";
+    for(i = 0; i<SizeofArray; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+    //assume first element of the array is max & min
+    max = array[0];
+    min = array[0];
+
+    for(i = 1; i < SizeofArray; i++)
+    {
+        if(array[i] > max)
+        {
+            max = array[i];
+        }
+        else if(array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    cout<<"\n\nMaximum element of the array is: ";
+    cout<<max <<'\n';
+
+    cout<<"\nMinimum element of the array is: ";
+    cout<<min<<"\n";
+    return 0;
+}
