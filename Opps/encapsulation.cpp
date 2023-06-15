413f5ef4e4eaa075a6fe8afdb84ac4ebcd5a369f
Kunal2703 <kunalsingh2703@gmail.com>
2023-06-15 14:09:55 +0530

    encapsulation.cpp

---
+++ Opps/encapsulation.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class Employee
+{
+    private:
+        int salary;
+    
+    public:
+        void setSalary(int s)
+        {
+            salary = s;
+        }
+        int getSalary()
+        {
+            return salary;
+        }
+};
+
+int main()
+{
+    Employee myObj;
+    myObj.setSalary(500000);
+    cout << myObj.getSalary() << endl;
+
+    return 0;
+}
