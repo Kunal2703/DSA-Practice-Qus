3511a6907c622641e86e2d9de97aa890e8fa982a
Kunal2703 <kunalsingh2703@gmail.com>
2023-06-15 12:54:48 +0530

    class in oops

---
+++ Opps/class.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+class MyClass   //The class
+{
+    public:     //Access Specifier
+        int myNum;  //Attribute(int variable)
+        string myString; //Attribute(string variable)
+};
